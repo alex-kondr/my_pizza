edabcce956d20fda17718c7111444df2665630d7
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-09-05 19:14:00 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    return render_template("index.html")
+
+
+@app.get('/results/')
+def results():
+    context = {
+        "max_score": 100,
+        "test_name": "Python Challenge",
+        "students": [
+        {"name": "Vlad", "score": 100},
+        {"name": "Sviatoslav", "score": 99},
+        {"name": "Юстин", "score": 100},
+        {"name": "Viktor", "score": 79},
+        {"name": "Ярослав", "score": 93},
+        ],
+        "title": "Результати тестування"
+    }
+
+    return render_template("results.html", **context)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
