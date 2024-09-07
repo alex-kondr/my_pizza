c75f953a79bd0818472b9fbe7a5c223330590e32
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-09-07 11:35:56 +0300

    save

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
+import random
+
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-
+CONTACT = "+380731570901"
 
 
 @app.get("/")
 def index():
-    
+    numbers = [random.randint(0, 100) for _ in range(10)]
+    return render_template("index.html", contact=CONTACT, numbers=numbers)
 
 
-    return render_template("index.html")
-
-
-@app.get('/results/')
+@app.get('/results-ababagalamaga/')
 def results():
     context = {
         "max_score": 100,
         "test_name": "Python Challenge",
         "students": [
         {"name": "Vlad", "score": 100},
+        {"name": "Vlad", "score": 42},
         {"name": "Sviatoslav", "score": 99},
         {"name": "Юстин", "score": 100},
         {"name": "Viktor", "score": 79},
         {"name": "Ярослав", "score": 93},
         ],
-        "title": "Результати тестування"
+        "title": "Результати тестування",
+        "contact": CONTACT
     }
 
     return render_template("results.html", **context)
